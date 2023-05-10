9f3d161e56e5c55231a2e51d81dd319496ca22d9
ManneKo13 <martin.korb@pccl.at>
2023-05-10 15:17:50 +0200

    Add more general approach to get all the filenames in the data folder

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import cv2 as cv
 from pathlib import Path
-from src.filenames import Filenames
+from src.data_preparation import Filenames
 import numpy as np
+from os import listdir
+from os import path
 
 def main():
+    cwd = Path.cwd().as_posix()
+    
+    filenames = Filenames(cwd)
+    num_pattern_files = len(filenames.subdir_filenames['pattern'])
+    img_pattern_files = []
+    for i in range(num_pattern_files):
+        filename = filenames.subdir_paths['pattern'] + '/' + filenames.subdir_filenames['pattern'][i] 
+        img = cv.imread(filename)
+        img_pattern_files.append(img)
+
     ''' ---------------
         Load some image
     '''
-    # img = cv.imread(data_root)
-    # cv.startWindowThread()
-    # cv.namedWindow('Display Window')
-    # cv.imshow('Display Window', img)
-    # cv.waitKey(0)
+    cv.startWindowThread()
+    cv.namedWindow('Display Window')
+    cv.imshow('Display Window', img_pattern_files[1])
+    # Press enter in the window to close 
+    cv.waitKey(0)
 
-    filenames = Filenames(Path.cwd().as_posix())
-    pattern_files = filenames.fn_pattern
-    img = cv.imread('')
-    pass
 
 if __name__ == "__main__":
     main()
---
+++ src/data_preparation.py
@@ -0,0 +1,41 @@
+import os
+import glob
+
+class Filenames():
+    def __init__(self, cwd: str) -> None:
+        data_dir = cwd + '/data'
+        data_subdir = os.listdir(data_dir)
+
+        # Only subdirectories?
+        subdir_names = []
+        subdir_names_slash = []
+        for i in range(len(data_subdir)):
+            if os.path.isdir(data_dir + '/' + data_subdir[i]):
+                # subdir_names_slash.append('/' + data_subdir[i])
+                subdir_names.append(data_subdir[i])
+
+        self.subdir_names = subdir_names
+
+        subdir_paths = {}
+        for i in range(len(subdir_names)):
+            subdir_paths[subdir_names[i]] = data_dir + '/' + subdir_names[i]
+        
+        self.subdir_paths = subdir_paths
+
+        # self.subdir_names_slash = subdir_names_slash
+
+        subdir_filenames = {}
+        for i in range(len(subdir_names)):
+            subdir_filenames[subdir_names[i]] = [os.path.basename(x) for x in glob.glob(self.subdir_paths[subdir_names[i]] + '/*.png')]
+        
+        self.subdir_filenames = subdir_filenames
+             
+        # # List of filenames in directory points
+        # self.fn_area = [os.path.basename(x) for x in glob.glob(data_dir + ls_subdir_data[0] + '/*.png')]
+        # # List of filenames in directory points
+        # self.fn_ok = [os.path.basename(x) for x in glob.glob(data_dir + ls_subdir_data[1] + '/*.png')]
+        # # List of filenames in directory points
+        # self.fn_pattern = [os.path.basename(x) for x in glob.glob(data_dir + ls_subdir_data[2] + '/*.png')]
+        # # List of filenames in directory points
+        # self.fn_points = [os.path.basename(x) for x in glob.glob(data_dir + ls_subdir_data[3] + '/*.png')]
+   
