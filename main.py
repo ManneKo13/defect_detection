94092be71f18420fb15afb87d25db61aba79c71c
ManneKo13 <martin.korb@pccl.at>
2023-05-10 12:44:33 +0200

    Load and show an examplary image

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import cv2 as cv
 from pathlib import Path
 
 def main():
-    data_root = Path.cwd().as_posix() + '/data/points'
-    pass    
+    data_root = Path.cwd().as_posix() + '/data/points/0_0#(2171, 1537).png'
+    ''' ---------------
+        Load some image
+    '''
+    img = cv.imread(data_root)
+    cv.startWindowThread()
+    cv.namedWindow('Display Window')
+    cv.imshow('Display Window', img)
+    cv.waitKey(0)
 
 if __name__ == "__main__":
     main()
