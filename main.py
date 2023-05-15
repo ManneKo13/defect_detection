fe566de85546a77f186b099a6bf62ce276123e2b
ManneKo13 <martin.korb@pccl.at>
2023-05-15 10:52:23 +0200

    further modifikations of the plotting functions for better readability

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import image_names
 import cv2 as cv
 from pathlib import Path
 from src.data_preparation import Filenames
@@ -8,29 +9,66 @@ import matplotlib
 matplotlib.use('TkAgg',force=True)
 from matplotlib import pyplot as plt
 
-def make_img_list(subdir_name: str) -> list:
+''' ---------------
+    Function to get lists of the desired files in given subdirectory
+    - img_pattern_files: list of images represented as numpy-arrays
+    - img_names: list of the names of the images as string
+'''
+def make_img_lists(subdir_name: str) -> list:
     cwd = Path.cwd().as_posix()
     
     filenames = Filenames(cwd)
+    img_names = filenames.subdir_filenames[subdir_name]
     num_pattern_files = len(filenames.subdir_filenames[subdir_name])
     img_pattern_files = []
     for i in range(num_pattern_files):
+        # Get the whole path-filename 
         filename = filenames.subdir_paths[subdir_name] + '/' + filenames.subdir_filenames[subdir_name][i] 
         img = cv.imread(filename)
         img_pattern_files.append(img)
 
-    return img_pattern_files
+    return img_pattern_files, img_names
 
-def plot2img(img1, img2):
+''' ---------------
+    Function to plot two images, where the images should
+    be represented as numpy-arrays. This function should be
+    used to compare those two images.
+    - img1: the original image
+    - img2: the modified image
+'''
+def plot2img(img1, img2, img_name):
+    rgb_img1 = cv.cvtColor(img1, cv.COLOR_BGR2RGB)
+    rgb_img2 = cv.cvtColor(img2, cv.COLOR_BGR2RGB)
     fig = plt.figure(figsize = (15, 10))
     ax1 = fig.add_subplot(121)
     ax2 = fig.add_subplot(122)
-    ax1.imshow(img1), ax1.set_title('Original')
-    ax2.imshow(img2), ax2.set_title('Output')
+    ax1.imshow(rgb_img1), ax1.set_title('Original (%s)' % img_name)
+    ax2.imshow(rgb_img2), ax2.set_title('Output')
+    plt.show()
+
+def plotHist(img, img_name):
+    ''' ---------------
+        Histogram for blue pixels
+        - hist is a 256x1 array, each value corresponds to number of pixels
+          in that image with its corresponding pixel value.    
+    '''
+    rgb_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    fig = plt.figure(figsize = (15, 10))
+    ax1 = fig.add_subplot(121) 
+    ax1.imshow(rgb_img), ax1.set_title('Original (%s)' % img_name)
+    # plt.xticks([]), plt.yticks([])  
+    # hist = cv.calcHist([rgb_img], [channel], None, [256], [0, 256])
+    ax2 = fig.add_subplot(122) 
+    color = ('b', 'g', 'r')
+    for i, col in enumerate(color):
+        hist = cv.calcHist([rgb_img], [i], None, [256], [0, 256])
+        ax2 = plt.plot(hist, color = col)
+        plt.xlim([0, 256])
+
     plt.show()
 
 def main():
-    img_pattern_files = make_img_list('pattern')
+    img_pattern_files, image_names = make_img_lists('pattern')
     ''' ---------------
         Plot some image
     '''
@@ -40,30 +78,12 @@ def main():
     # # Press enter in the window to close 
     # cv.waitKey(0)
 
-    ''' ---------------
-        Histogram for blue pixels
-        - hist is a 256x1 array, each value corresponds to number of pixels
-          in that image with its corresponding pixel value.    
-    '''
-    # rgb_img = cv.cvtColor(img_pattern_files[0], cv.COLOR_BGR2RGB)
-    # plt.subplot(121), plt.imshow(rgb_img), plt.title('Original')
-    # plt.xticks([]), plt.yticks([])  
-    # hist = cv.calcHist([img_pattern_files[0]], [0], None, [256], [0, 256])
-    # plt.subplot(122), plt.plot(hist, 'b')
-    # plt.xlim([0, 256])
-    # # color = ('b', 'g', 'r')
-    # # for i, col in enumerate(color):
-    # #     hist = cv.calcHist([img_pattern_files[0]], [i], None, [256], [0, 256])
-    # #     plt.plot(hist, color = col)
-    # #     plt.xlim([0, 256])
-    # plt.show()
-
+    plotHist(img_pattern_files[4], image_names[1])
     ''' ---------------
         Using some Filters
     '''
-    rgb_img = cv.cvtColor(img_pattern_files[0], cv.COLOR_BGR2RGB)
-    laplacian = cv.Laplacian(rgb_img, , 5)
-    plot2img(rgb_img, laplacian)
+    # mod_img = cv.cvtColor(img_pattern_files[0], cv.COLOR_BGR2HSV)
+    # plot2img(img_pattern_files[0], mod_img, image_names[0])
     
 
 if __name__ == "__main__":
